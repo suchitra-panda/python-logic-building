69d8a62c3d341273eda4390d5615567d632a57da
suchitra-panda <pndsuchitra@gmail.com>
2026-01-28 21:07:21 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,19 @@
+
+
+# Problem 1: Sum of two numbers
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("Sum:", a + b)
+
+print("----------------")
+
+# Problem 2: Square of a number
+a = int(input("Enter a number: "))
+print("Square:", a * a)
+
+print("----------------")
+
+# Problem 3: Name and Age
+name = input("Enter name: ")
+age = int(input("Enter age: "))
+print(f"My name is {name} and I am {age} years old")
